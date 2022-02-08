0adf7bd1459f69fe64ec0906e6f004051ea6b9db
ivy <1063201840@qq.com>
2022-02-08 11:05:38 +0800

    build

--- src/views/login/component/loginComponent.tsx
+++ src/views/login/component/loginComponent.tsx
@@ -183,9 +183,9 @@ export default class LoginComponent extends Component {
                 </Form>
                 {loginSwitch?
                     <div className='forget flexb'>
-                        <Link to='/APP/register/forget'><span>忘记密码？</span></Link>
+                        <Link to='/app/register/forget'><span>忘记密码？</span></Link>
                         
-                        <Link to='/APP/register/register'><span>免费注册</span></Link>
+                        <Link to='/app/register/register'><span>免费注册</span></Link>
                     </div>:
                     <div></div>
                 }
